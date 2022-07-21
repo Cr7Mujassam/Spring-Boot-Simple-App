04d94b350a892bd117fae5f2b7d39ad2beb6eb95
Cr7Mujassam <106069072+Cr7Mujassam@users.noreply.github.com>
2022-07-21 12:24:04 +0530

    Update loginController.java

--- loginController.java
+++ loginController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // </login =>> "Welcome to login Page"
-
+// It will pick the class by springboot
 @Controller
 public class loginController {
 	
+	//It will map the code to url
 	@RequestMapping("/login")
+	//It will print the data of the method
 	@ResponseBody
 	public String login(){
 		return "Welcome to login Page" + " " + "Browser!!!";
